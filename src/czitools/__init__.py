75f340402667b1bb8b2acc3c2a22967666631900
sebi06 <sebrhode@gmail.com>
2022-07-07 21:26:46 +0200

    new package structure

---
+++ src/czitools/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+# version of the czitools package
+__version__ = "0.0.9"
+
+from .metadata import pylibczirw_metadata
+from .utils import misc, napari_tools
+from .imagedata import pylibczirw_tools
