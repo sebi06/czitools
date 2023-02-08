11d279f0099593f598d2d2cf69aa75bd1c65e454
sebi06 <3833249+sebi06@users.noreply.github.com>
2023-02-08 16:21:42 +0100

    updated test_czimd_box.py

--- src/czitools/_tests/test_czimd_box.py
+++ src/czitools/_tests/test_czimd_box.py
@@ -1,62 +1,68 @@
 from czitools import pylibczirw_metadata as czimd
 from pathlib import Path, PurePath
+import pytest
+from typing import List, Dict, Tuple, Optional, Type, Any, Union, Mapping
 
 
 # adapt to your needs
 defaultdir = Path(__file__).resolve().parents[3] / "data"
 
-def test_general_attr():
+
+@pytest.mark.parametrize(
+    "attribute",
+    [
+        "has_customattr",
+        "has_exp",
+        "has_disp",
+        "has_hardware",
+        "has_scale",
+        "has_instrument",
+        "has_microscopes",
+        "has_detectors",
+        "has_objectives",
+        "has_tubelenses",
+        "has_disp",
+        "has_channels",
+        "has_info",
+        "has_app",
+        "has_doc",
+        "has_image",
+        "has_scenes",
+        "has_dims"
+    ]
+)
+def test_general_attr(attribute: str) -> None:
 
     filepath = defaultdir / "CellDivision_T=3_Z=5_CH=2_X=240_Y=170.czi"
 
     czibox = czimd.get_czimd_box(filepath)
 
-    assert(hasattr(czibox, "has_customattr"))
-    assert(hasattr(czibox, "has_exp = False"))
-    assert(hasattr(czibox, "has_disp"))
-    assert(hasattr(czibox, "has_hardware"))
-    assert(hasattr(czibox, "has_scale"))
-    assert(hasattr(czibox, "has_instrument"))
-    assert(hasattr(czibox, "has_microscopes"))
-    assert(hasattr(czibox, "has_detectors"))
-    assert(hasattr(czibox, "has_objectives"))
-    assert(hasattr(czibox, "has_tubelenses"))
-    assert(hasattr(czibox, "has_disp"))
-    assert(hasattr(czibox, "has_channels"))
-    assert(hasattr(czibox, "has_info"))
-    assert(hasattr(czibox, "has_app"))
-    assert(hasattr(czibox, "has_doc"))
-    assert(hasattr(czibox, "has_image"))
-    assert(hasattr(czibox, "has_scenes"))
-    assert(hasattr(czibox, "has_dims"))
+    assert (hasattr(czibox, attribute))
 
 
-def test_box():
+@pytest.mark.parametrize(
+    "czifile, expected",
+    [
+        ("CellDivision_T=3_Z=5_CH=2_X=240_Y=170.czi", True),
+        ("Al2O3_SE_020_sp.czi", True),
+        ("w96_A1+A2.czi", True),
+        ("Airyscan.czi", True),
+        ("newCZI_zloc.czi", True),
+        ("does_not_exist.czi", False),
+        ("FOV7_HV110_P0500510000.czi", True),
+        ("Tumor_HE_RGB.czi", True),
+        ("WP96_4Pos_B4-10_DAPI.czi", True),
+        ("S=3_1Pos_2Mosaic_T=2=Z=3_CH=2_sm.czi", True)
+    ]
+)
+def test_box(czifile: List[str], expected: bool) -> None:
 
-    czifiles = ["CellDivision_T=3_Z=5_CH=2_X=240_Y=170.czi",
-                "Al2O3_SE_020_sp.czi",
-                "w96_A1+A2.czi",
-                "Airyscan.czi",
-                "newCZI_zloc.czi",
-                "does_not_exist.czi",
-                "FOV7_HV110_P0500510000.czi",
-                "Tumor_HE_RGB.czi",
-                "WP96_4Pos_B4-10_DAPI.czi",
-                "S=3_1Pos_2Mosaic_T=2=Z=3_CH=2_sm.czi"
-                ]
+    filepath = defaultdir / czifile
 
-    results = [True, True, True, True, True, False, True, True, True, True]
+    try:
+        czibox = czimd.get_czimd_box(filepath)
+        ok = True
+    except Exception:
+        ok = False
 
-    for czifile, result in zip(czifiles, results):
-
-        filepath = defaultdir / czifile
-
-        try:
-            czibox = czimd.get_czimd_box(filepath)
-            ok = True
-        except Exception:
-            ok = False
-
-        assert(ok == result)
-
-test_box()
+    assert (ok == expected)
