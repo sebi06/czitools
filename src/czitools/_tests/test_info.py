517eaedaf3a0b946aeedf069a5d02f440c59b86b
sebi06 <sebrhode@gmail.com>
2024-01-04 16:28:22 +0100

    Updates

--- src/czitools/_tests/test_info.py
+++ src/czitools/_tests/test_info.py
@@ -9,32 +9,29 @@ basedir = Path(__file__).resolve().parents[3]
 @pytest.mark.parametrize(
     "czifile, result",
     [
-        ("CellDivision_T=3_Z=5_CH=2_X=240_Y=170.czi", ['ZEN 3.2 (blue edition)',
-                                                             '3.2.0.00001',
-                                                             '2016-02-12T09:41:02.4915604Z',
-                                                             True,
-                                                             'CellDivision_T=3_Z=5_CH=2_X=240_Y=170.czi']),
-        
-        ("Airyscan.czi", ['ZEN (blue edition)',
-                                '3.5.093.00000',
-                                None,
-                                True,
-                                'Airyscan.czi']),
-        
-        ("newCZI_zloc.czi", ['pylibCZIrw',
-                                   '3.2.1',
-                                   None,
-                                   True,
-                                   'newCZI_zloc.czi'])
-    ]
+        (
+            "CellDivision_T=3_Z=5_CH=2_X=240_Y=170.czi",
+            [
+                "ZEN 3.2 (blue edition)",
+                "3.2.0.00001",
+                "2016-02-12T09:41:02.4915604Z",
+                True,
+                "CellDivision_T=3_Z=5_CH=2_X=240_Y=170.czi",
+            ],
+        ),
+        (
+            "Airyscan.czi",
+            ["ZEN (blue edition)", "3.5.093.00000", None, True, "Airyscan.czi"],
+        ),
+        ("newCZI_zloc.czi", ["pylibCZIrw", "3.2.1", None, True, "newCZI_zloc.czi"]),
+    ],
 )
 def test_information(czifile: str, result: List) -> None:
-
     filepath = basedir / "data" / czifile
     md = czimd.CziMetadata(filepath)
 
-    assert (md.software_name == result[0])
-    assert (md.software_version == result[1])
-    assert (md.acquisition_date == result[2])
-    assert (Path.exists(Path(md.dirname)) == result[3])
-    assert (md.filename == result[4])
+    assert md.software_name == result[0]
+    assert md.software_version == result[1]
+    assert md.acquisition_date == result[2]
+    assert Path.exists(Path(md.dirname)) == result[3]
+    assert md.filename == result[4]
--- src/czitools/_tests/test_napari_tools.py
+++ src/czitools/_tests/test_napari_tools.py
@@ -1,6 +1,11 @@
 import pytest
-from czitools import napari_tools
+from czitools import napari_tools, metadata_tools, read_tools
+import numpy as np
+import napari
+from pathlib import Path
 from typing import List, Dict, Tuple, Optional, Type, Any, Union, Mapping
+
+basedir = Path(__file__).resolve().parents[3]
 
 
 @pytest.mark.parametrize(
@@ -19,3 +24,51 @@ def test_rename_sliders(
 
     # Assert
     assert renamed_sliders == expected_sliders
+
+
+@pytest.mark.parametrize(
+    "czifile, num_layers",
+    [
+        ("w96_A1+A2.czi", 2),
+    ],
+)
+def test_show_image(czifile: str, num_layers: int) -> None:
+    """Test that the `show` function correctly displays a two-channel image."""
+
+    filepath = basedir / "data" / czifile
+    md = metadata_tools.CziMetadata(filepath)
+
+    array6d, mdata, dim_string6d = read_tools.read_6darray(
+        filepath,
+        output_order="STCZYX",
+        use_dask=False,
+        chunk_zyx=False,
+    )
+
+    # Create a viewer
+    viewer = napari.Viewer()
+
+    # Display the image
+    layers = napari_tools.show(
+        viewer,
+        array6d,
+        mdata,
+        dim_string=dim_string6d,
+        blending="additive",
+        contrast="from_czi",
+        gamma=0.85,
+        show_metadata="tree",
+        name_sliders=True,
+    )
+
+    # Check that the layer is present in the viewer
+    assert len(viewer.layers) == num_layers
+
+    for layer in range(num_layers):
+        # Check that the layer is a `napari.layers.Image` layer
+        assert isinstance(viewer.layers[layer], napari.layers.Image)
+
+        # Check that the layer's data is the same as the input image
+        np.testing.assert_array_equal(viewer.layers[layer].data, array6d[:, :, layer:layer+1, :, :, :])
+
+    viewer.close()
