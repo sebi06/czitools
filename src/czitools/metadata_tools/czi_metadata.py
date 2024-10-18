3dac7a040f6bcbf6c3c05a790a39cad2217af898
sebi06 <sebrhode@gmail.com>
2024-10-18 16:14:19 +0200

    updates

--- src/czitools/metadata_tools/czi_metadata.py
+++ src/czitools/metadata_tools/czi_metadata.py
@@ -88,6 +88,8 @@ class CziMetadata:
             Checks if all scenes have the same shape.
         check_if_rgb(pixeltypes: Dict) -> Tuple[bool, bool]:
             Checks if the image is RGB and if pixel types are consistent.
+    """
+
     filepath: Union[str, os.PathLike[str]]
     filename: Optional[str] = field(init=False, default=None)
     dirname: Optional[str] = field(init=False, default=None)
@@ -133,7 +135,6 @@ class CziMetadata:
         init=False, default_factory=lambda: ()
     )
     verbose: bool = False
-    """
 
     def __post_init__(self):
         if validators.url(str(self.filepath)):
