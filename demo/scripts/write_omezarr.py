e52624ba523a1b79e1b224abea0e78700ebc0943
sebi06 <sebrhode@gmail.com>
2025-07-18 16:05:42 +0200

    updates

--- demo/scripts/write_omezarr.py
+++ demo/scripts/write_omezarr.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 #################################################################
-# File        : use_write_tools.py
+# File        : write_omezarr.py
 # Author      : sebi06
 #
 # Disclaimer: This code is purely experimental. Feel free to
@@ -10,15 +10,24 @@
 #################################################################
 
 from czitools.read_tools import read_tools
-from czitools.write_tools import write_tools
 from czitools.metadata_tools import czi_metadata as czimd
-import napari
 from pathlib import Path
+import ngff_zarr as nz
+import zarr
+import shutil
+
+# this script required ngff-zarr[validate, dask-image] to be installed
+print(f"ZARR Verion: {zarr.__version__} NGFF-ZARR Version: {nz.__version__}")
 
 # open s simple dialog to select a CZI file
 defaultdir = Path(Path(__file__).resolve().parents[2]) / "data"
 filepath = defaultdir / "CellDivision_T3_Z5_CH2_X240_Y170.czi"
 zarr_path = Path(str(filepath)[:-4] + ".ome.zarr")
+
+# optional: check if path exists
+remove = True
+if zarr_path.exists() and remove:
+    shutil.rmtree(zarr_path, ignore_errors=False, onerror=None)
 
 # get the metadata_tools at once as one big class
 mdata = czimd.CziMetadata(filepath)
@@ -26,19 +35,18 @@ print("Number of Scenes: ", mdata.image.SizeS)
 scene_id = 0
 
 # return a 6D array with dimension order STCZYX(A)
-array, mdata = read_tools.read_6darray(filepath, use_dask=True)
+array, mdata = read_tools.read_6darray(filepath)
 array = array[scene_id, ...]
 
-# write OME-ZARR using utility function
-zarr_path = write_tools.write_omezarr(
-    array, zarr_path=str(zarr_path), axes="tczyx", overwrite=True
+# create NGFF image from the array
+image = nz.to_ngff_image(
+    array.data,
+    dims=["t", "c", "z", "y", "x"],
+    scale={"y": mdata.scale.Y, "x": mdata.scale.X, "z": mdata.scale.Z},
+    name=mdata.filename,
 )
 
-if array is None:
-    print("Empty array. Nothing to display in Napari")
-else:
-    # show OME-ZARR inside napari viewer
-    viewer = napari.Viewer()
-    viewer.open(zarr_path)
+print(f"NGFF Image: {image}")
 
-    napari.run()
+# create multi-scaled, chunked data structure from the image
+multiscales = nz.to_multiscales(image, [2, 4], method=nz.Methods.DASK_IMAGE_GAUSSIAN)
