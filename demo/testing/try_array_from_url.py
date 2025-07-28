20cff4c70f6602d7efda3e1206ef7706006b2445
sebi06 <sebrhode@gmail.com>
2025-07-28 18:02:20 +0200

    updates

---
+++ demo/testing/try_array_from_url.py
@@ -0,0 +1,8 @@
+from czitools.read_tools import read_tools
+
+url = r"https://github.com/sebi06/napari-czitools/raw/main/src/napari_czitools/sample_data/CellDivision_T15_Z20_CH2_X600_Y500_DCV_ZSTD.czi"
+
+# return an array with dimension order STCZYX(A)
+array6d, mdata = read_tools.read_6darray(url)
+
+print(f"Array shape: {array6d.shape}")
